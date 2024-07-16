89aeb098c85938e9213975c82195504d1a2de04e
MarcosRodrigoT <marcosrodrigo5@hotmail.com>
2024-07-16 20:57:19 +0200

    Add L1 & L2 regularization loss for attention
    Before this I was doing torch.sum(att_scores...)

--- model/train.py
+++ model/train.py
@@ -60,21 +60,24 @@ def load_hyperparameters(adapter):
         batch_size = 200
         epochs = 201
         lr = 1e-5
-        lambda_reg = 0.001
+        lambda_reg_l1 = 0.001
+        lambda_reg_l2 = 0.001
     elif adapter == "conv1d":
         batch_size = 128
         epochs = 501
         lr = 5e-4
-        lambda_reg = 0.001
+        lambda_reg_l1 = 0.001
+        lambda_reg_l2 = 0.001
     elif adapter == "transformer":
         batch_size = 64
         epochs = 301
         lr = 5e-4
-        lambda_reg = 0.001
-    return batch_size, epochs, lr, lambda_reg
+        lambda_reg_l1 = 0.001
+        lambda_reg_l2 = 0.001
+    return batch_size, epochs, lr, lambda_reg_l1, lambda_reg_l2
 
 
-def train(dataloader, adapter, device, epochs=100, lr=0.001, lambda_reg=0.1):
+def train(dataloader, adapter, device, epochs=100, lr=0.001, lambda_reg_l1=0.1, lambda_reg_l2=0.1):
     # Initialize the network
     adapt_net = load_model(adapter).to(device)
     adapt_net = nn.DataParallel(adapt_net)  # Wrap the model for data parallelism
@@ -86,6 +89,8 @@ def train(dataloader, adapter, device, epochs=100, lr=0.001, lambda_reg=0.1):
         total_epoch_loss = 0
         l2_epoch_loss = 0
         reg_epoch_loss = 0
+        reg_epoch_loss_l1 = 0
+        reg_epoch_loss_l2 = 0
 
         for batch in tqdm(dataloader, desc="Batches", leave=False):
             videos, text_descriptions = batch
@@ -97,11 +102,11 @@ def train(dataloader, adapter, device, epochs=100, lr=0.001, lambda_reg=0.1):
 
             # Forward pass
             att_scores = adapt_net(padded_videos.permute(0, 2, 1), masks)  # (B, F, 1)
-            # Fix: As of now, This layer produces a matrix of numbers between 0 and 1 (sigmoid).
+            # TODO: As of now, This layer produces a matrix of numbers between 0 and 1 (sigmoid).
             # This means that regularizing this layer as we are doing below with torch.sum(att_scores...) only serves to produce small numbers, not few 1's as was our intention.
             # This makes the regularization we are doing here useless, as we still have to binarize these scores later on.
             # If we binarize scores here, we would need to provide a desired summary length...
-            # TODO: Find alternative to this.
+            # Find alternative to this.
 
             # Compute Out = M @ Att
             Out = torch.matmul(padded_videos.permute(0, 2, 1), att_scores).squeeze(-1)  # shape (B, E)
@@ -110,9 +115,15 @@ def train(dataloader, adapter, device, epochs=100, lr=0.001, lambda_reg=0.1):
             loss = criterion(Out, text_descriptions)  # (B, E)
             loss = loss.mean(dim=1)  # Reduce over features dimension (B)
             loss = (loss * masks[:, 0].to(device)).mean()  # Mask out padded frames (scalar)
+            # Fix: The L2-norm is not the same as the MSE loss. I need to compute ||Out - T_G||_2^2
+            # -> First: compute Diff = Out - T_G
+            # -> Second: compute the L2-norm of Diff
+            # -> Third: square it
 
-            # Add regularization term (sum of attention scores)
-            reg_term = lambda_reg * torch.sum(att_scores.squeeze(-1))
+            # Add L1 and L2 regularization terms
+            l1_reg = lambda_reg_l1 * torch.sum(torch.abs(att_scores.squeeze(-1)))
+            l2_reg = lambda_reg_l2 * torch.sum(att_scores.squeeze(-1) ** 2)
+            reg_term = l1_reg + l2_reg
             total_loss = loss + reg_term
 
             # Backward pass and optimization
@@ -123,12 +134,19 @@ def train(dataloader, adapter, device, epochs=100, lr=0.001, lambda_reg=0.1):
             total_epoch_loss += total_loss.item()
             l2_epoch_loss += loss.item()
             reg_epoch_loss += reg_term.item()
+            reg_epoch_loss_l1 += l1_reg.item()
+            reg_epoch_loss_l2 += l2_reg.item()
 
         if epoch % 10 == 0:
             loss_t = total_epoch_loss / len(dataloader)
             loss_l = l2_epoch_loss / len(dataloader)
             loss_r = reg_epoch_loss / len(dataloader)
-            tqdm.write(f"Epoch [{epoch: >3}/{epochs}], Total Loss: {loss_t:.5f}\t --- L2 Loss: {loss_l:.5f}\t --- Reg Loss: {loss_r:.5f}")
+            loss_r_l1 = reg_epoch_loss_l1 / len(dataloader)
+            loss_r_l2 = reg_epoch_loss_l2 / len(dataloader)
+            tqdm.write(
+                f"Epoch [{epoch: >3}/{epochs}], Total Loss: {loss_t: >12.5f} --- MSE Loss: {loss_l: >12.5f} --- Reg Loss:"
+                f" {loss_r: >12.5f} --- Reg Loss L1: {loss_r_l1: >12.5f} --- Reg Loss L2: {loss_r_l2: >12.5f}"
+            )
 
     # Save the trained model
     model_save_path = f"model/adapter_{adapter}_model.pth"
@@ -144,7 +162,7 @@ if __name__ == "__main__":
     adapter = "mlp"  # "mlp" / "conv1d" / "transformer"
 
     # Hyperparameters
-    batch_size, epochs, lr, lambda_reg = load_hyperparameters(adapter)
+    batch_size, epochs, lr, lambda_reg_l1, lambda_reg_l2 = load_hyperparameters(adapter)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load the data
@@ -156,4 +174,4 @@ if __name__ == "__main__":
     # [(video1, video2), (text1, text2)]
 
     # Train the model
-    train(dataloader, adapter, device, epochs=epochs, lr=lr, lambda_reg=lambda_reg)
+    train(dataloader, adapter, device, epochs=epochs, lr=lr, lambda_reg_l1=lambda_reg_l1, lambda_reg_l2=lambda_reg_l2)
